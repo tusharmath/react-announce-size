cd600ac088f40cceda9127dd084d99e3c10be7ed
Tushar Mathur <tusharmath@gmail.com>
2015-12-26 11:42:05 +0530

    load react-dom in production only

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 const createDeclarative = require('react-announce').createDeclarative
 const createStoreAsStream = require('reactive-storage').createStoreAsStream
-const ReactDOM = require('react-dom')
 const Rx = require('rx')
 const Seamless = require('seamless-immutable')
 const _ = require('lodash')
@@ -14,7 +13,7 @@ const Subject = Rx.BehaviorSubject
 
 const defaultParams = {
   getResizeStream: () => Rx.Observable.fromEvent(window, 'resize'),
-  findDOMNode: x => ReactDOM.findDOMNode(x)
+  findDOMNode: x => require('react-dom').findDOMNode(x)
 }
 
 const globalStream = new Subject()
