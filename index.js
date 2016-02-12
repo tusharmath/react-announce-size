83d34f36a5400d958a32e63c29a54ab6999b4a13
Tushar Mathur <tusharmath@gmail.com>
2016-02-12 20:39:13 +0530

    feat(size): add size decorator
    size decorator dispatches a custom event RESIZE on the component's stream

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  */
 
 'use strict'
-
+const e = exports
 const ReactDOM = require('react-dom')
+e.ReactDOM = ReactDOM
 const Rx = require('rx')
+const createDeclarative = require('react-announce').createDeclarative
 const PROPS = ['top', 'bottom', 'left', 'right', 'height', 'width']
 
 // TODO: Add better tests for using global functions
@@ -14,6 +16,7 @@ const defaultParams = {
   getResizeStream: () => Rx.Observable.fromEvent(window, 'resize'),
   findDOMNode: x => ReactDOM.findDOMNode(x)
 }
+
 const pick = (obj, keys) => {
   const out = {}
   keys
@@ -22,9 +25,38 @@ const pick = (obj, keys) => {
   return out
 }
 
-const rectToString = x => PROPS.map(i => x[i]).join(':')
 
-exports.createSizeStore = exports.create = params => {
+e.select = (size, component) => ({size, component})
+
+e.rectToString = x => PROPS.map(i => x[i]).join(':')
+
+e.getComponentStream = x => x
+    .filter(x => ['DID_MOUNT', 'DID_UPDATE'].indexOf(x.event) > -1)
+    .pluck('component')
+
+e.getResizeStream = window => Rx.Observable.fromEvent(window, 'resize')
+
+e.getComponentSizeStream = (ReactDOM, resize, component) => component
+    .combineLatest(resize, a => a)
+    .map(x => ReactDOM.findDOMNode(x).getBoundingClientRect())
+    .distinctUntilChanged(e.rectToString)
+
+e.dispatchSize = x => x.component.dispatch('RESIZE', x.size)
+
+e.bindToStream = (d, stream, window) => {
+  const resize = d.getResizeStream(window)
+  const component = e.getComponentStream(stream)
+  const componentSizeStream = e.getComponentSizeStream(d.ReactDOM, resize, component)
+  return componentSizeStream.withLatestFrom(component, e.select)
+    .subscribe(d.dispatchSize)
+}
+
+e.size = createDeclarative(function (stream, dispose, window) {
+  dispose(bindToStream(e, stream, window))
+})
+
+// TODO: Deprecate functionality
+e.createSizeStore = e.create = params => {
   const i = Object.assign({}, defaultParams, params)
   const sizeStore = new Rx.BehaviorSubject({})
   const componentStream = new Rx.Subject()
@@ -41,7 +73,7 @@ exports.createSizeStore = exports.create = params => {
     .pluck('size')
     .subscribe(size => sizeStore.onNext(pick(size, PROPS)))
   return {
-    getStream: () => sizeStore.distinctUntilChanged(rectToString).filter(x => x.top),
+    getStream: () => sizeStore.distinctUntilChanged(e.rectToString).filter(x => x.top),
     sync: () => componentStream
   }
 }
